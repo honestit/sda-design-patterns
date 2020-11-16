0c21b52a80b332198c8581bbbf86bcabd68cb5f5
honestit <michal.kupisinski@gmail.com>
2020-11-16 19:15:40 +0100

    Moduł A. Builder. 1

--- modules/module_a/src/main/java/gof/builder/BuilderApp.java
+++ modules/module_a/src/main/java/gof/builder/BuilderApp.java
@@ -1,8 +1,29 @@
 package gof.builder;
 
+import java.util.ArrayList;
+
 public class BuilderApp {
 
     public static void main(String[] args) {
+
+        Country poland = new Country();
+        poland.setName("Polska");
+        poland.setPopulation(38_000_000L);
+
+        City warsaw = new City();
+        warsaw.setName("Warszawa");
+        warsaw.setPopulation(1_600_000L);
+
+        warsaw.setCountry(poland);
+        poland.setCapital(warsaw);
+        poland.setCities(new ArrayList<>());
+        poland.getCities().add(warsaw);
+
+        City wroclaw = new City();
+        wroclaw.setName("Wrocław");
+        wroclaw.setPopulation(650_000L);
+
+        poland.getCities().add(wroclaw);
 
     }
 }

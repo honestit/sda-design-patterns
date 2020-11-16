47ebb393223b93f6f9b9950194ee9ab751c4927f
honestit <michal.kupisinski@gmail.com>
2020-11-16 20:27:47 +0100

    Moduł A. Factory Method. 1

--- modules/module_a/src/main/java/gof/factory_method/FactoryMethodApp.java
+++ modules/module_a/src/main/java/gof/factory_method/FactoryMethodApp.java
@@ -4,5 +4,27 @@ public class FactoryMethodApp {
 
     public static void main(String[] args) {
 
+        Trainee janNowak = new Trainee("Jan", "Nowak");
+        Trainee zosiaWojt = new Trainee("Zosia", "Wójt");
+
+        Manager lukasz = new Manager();
+        lukasz.setFirstName("Lukasz");
+        lukasz.setLastName("Kowalski");
+
+        janNowak.setManager(lukasz);
+        zosiaWojt.setManager(lukasz);
+
+        Coach coach = new Coach();
+        coach.setFirstName("Michał");
+        coach.setLastName("K.");
+        coach.setYearOfExperience(10);
+
+        Coach coach2 = new Coach();
+        coach.setFirstName("Kuba");
+        coach.setLastName("Z.");
+        coach.setYearOfExperience(5);
+
+
+
     }
 }

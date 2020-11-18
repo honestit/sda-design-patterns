3e3a4cf96c55a78b3745567d377886a5cde3af32
honestit <michal.kupisinski@gmail.com>
2020-11-18 17:22:35 +0100

    Moduł A. Decorator. 3

--- modules/module_a/src/main/java/gof/decorator/DecoratorApp.java
+++ modules/module_a/src/main/java/gof/decorator/DecoratorApp.java
@@ -1,12 +1,18 @@
 package gof.decorator;
 
+import gof.decorator.impl.AuditableBankAccount;
 import gof.decorator.impl.LoggableBankAccount;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class DecoratorApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BankAccount account1 = new InMemoryBankAccount();
         account1.open("Jan", "kowalski", "22222222222");
 
@@ -30,10 +36,29 @@ public class DecoratorApp {
         doubleLoggedAccount.open("0", "1", "2");
 
         closeAccount(doubleLoggedAccount);
+
+        AuditableBankAccount.asAuditable(new LoggableBankAccount(new InMemoryBankAccount()));
+        new LoggableBankAccount(AuditableBankAccount.asAuditable(new InMemoryBankAccount()));
+        BankAccount someSuspisiousAccount = AuditableBankAccount.asAuditable(new InMemoryBankAccount());
+        someSuspisiousAccount.open("Groźny", "Przestępca", "000000000000");
+        someSuspisiousAccount.deposit(BigDecimal.valueOf(1000));
+        someSuspisiousAccount.withdraw(BigDecimal.valueOf(999));
+
+        closeAccount(someSuspisiousAccount);
+
+        PrintWriter writer = new PrintWriter(new FileWriter(new File("abc.txt")));
     }
 
     public static void closeAccount(BankAccount someBankAccount) {
         BigDecimal deposit = someBankAccount.close();
         System.out.println("Pozostało z konta " + someBankAccount.getIBAN() + ": " + deposit);
+
+        if (someBankAccount instanceof AuditableBankAccount) {
+            // AuditableBankAccount auditable = (AuditableBankAccount) someBankAccount;
+            List<String> history = ((AuditableBankAccount) someBankAccount).getHistory();
+            for (String audit : history) {
+                System.out.println("===> " + audit);
+            }
+        }
     }
 }
